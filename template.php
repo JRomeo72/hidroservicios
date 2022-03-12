8f2f1cc856cf1021ebe9a68b7e0f61dc4dc1b440
Romeo Salomón <jromeo.dev@gmail.com>
2022-03-12 11:18:57 -0600

    Se modifico el style.css

--- template.php
+++ template.php
@@ -11,13 +11,13 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-		<meta http-equiv="cache-control" content="max-age=0" />
+		<!-- <meta http-equiv="cache-control" content="max-age=0" />
 		<meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
 		<meta http-equiv="expires" content="0" />
 		<meta http-equiv="Last-Modified" content="0">
 		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
-		<meta http-equiv="pragma" content="no-cache" />
-		
+		<meta http-equiv="pragma" content="no-cache" /> -->
+
 		<title>Hidroservicios</title>
 
 		<?php include "./components/Links.php" ?>
