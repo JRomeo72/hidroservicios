7235b83c2ef6c88a4276856fdf98d7cd1523b468
Romeo Salomón <jromeo.dev@gmail.com>
2022-03-14 23:23:43 -0600

    Correcciones en incompatibilidad de CSS

--- components/Links.php
+++ components/Links.php
@@ -1,3 +1,4 @@
+<link rel="icon" type="image/png" href="./favicon.png" sizes="48x48">
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="./assets/css/animate.css">
 <link rel="stylesheet" href="./assets/css/universal-parallax.min.css">
